4385c319dbe40cf74f474bf8a5ec163616cd2a0f
Andrey <notmymaster@gmail.com>
2023-02-18 14:51:12 +0200

    init with code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rust-tlgrm-bot-with-chat
+
+This bot uses the OpenAI API to generate a response to any message it receives from a user. It sends the message to the API, and then sends the generated text back to the user. You can customize the parameters of the API request to control the length and style of the generated text. Note that you will need to include the openai crate in your Cargo.toml file to use the OpenAI API.
---
+++ main.rs
@@ -0,0 +1,47 @@
+use futures::StreamExt;
+use telegram_bot::*;
+use tokio;
+
+const OPENAI_API_KEY: &str = "YOUR_API_KEY";
+
+async fn generate_text(api: Api, message: Message) {
+    let text = message.text.unwrap();
+
+    // Call the OpenAI API to generate text
+    let response = openai::CompletionBuilder::default()
+        .engine("davinci")
+        .prompt(text)
+        .max_tokens(1024)
+        .n(1)
+        .temperature(0.5)
+        .build()
+        .unwrap()
+        .send(OPENAI_API_KEY)
+        .await
+        .unwrap();
+
+    // Extract the generated text from the API response
+    let generated_text = response.choices[0].text.clone();
+
+    // Send the generated text back to the user
+    api.send(SendMessage::new(message.chat.id(), generated_text))
+        .await
+        .unwrap();
+}
+
+#[tokio::main]
+async fn main() -> Result<(), telegram_bot::Error> {
+    // Replace YOUR_BOT_TOKEN with your actual bot token
+    let api = Api::new("YOUR_BOT_TOKEN");
+
+    let mut stream = api.stream();
+
+    while let Some(update) = stream.next().await {
+        // If the update contains a new message
+        if let Some(message) = update.message {
+            tokio::spawn(generate_text(api.clone(), message));
+        }
+    }
+
+    Ok(())
+}
